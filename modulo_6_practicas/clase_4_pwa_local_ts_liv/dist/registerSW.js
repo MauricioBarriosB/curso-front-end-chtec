3f680e0c099a859f375375d0581b81a9f698d4a3
MauricioBarriosB <afasia.band@gmail.com>
2025-03-12 15:45:33 -0300

    124th commit

--- modulo_6_practicas/clase_4_pwa_local_ts_liv/dist/registerSW.js
+++ modulo_6_practicas/clase_4_pwa_local_ts_liv/dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://mauriciobarriosb.github.io/curso-front-end-chtec/modulo_6_practicas/clase_4_pwa_local_ts_liv/dist/sw.js', { scope: './' })})}
